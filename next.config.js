98348ff755fae5e78704f159b057e09cf977ffbd
HaiderInamWorkSpace <102531513+HaiderInamWorkSpace@users.noreply.github.com>
2022-07-29 05:54:37 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-    images: {
-    domains: ...,
-    path: `${basePath}/_next/image`,
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
