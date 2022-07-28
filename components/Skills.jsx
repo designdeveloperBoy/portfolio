44537c398a07ad373137a48ee3d52b7d8e2c5123
HaiderInamWorkSpace <102531513+HaiderInamWorkSpace@users.noreply.github.com>
2022-07-29 05:33:31 +1000

    Changed Image tag to img from line#16

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -13,7 +13,7 @@ const Skills = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src='/../public/assets/skills/html.png' height='64px' width='64px' alt='/' />
+                            <img src='/../public/assets/skills/html.png' height='64px' width='64px' alt='/' />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3>HTML</h3>
