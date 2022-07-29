2f85f04d9393fd8bc63836e1533c924c99dff2c5
HaiderInamWorkSpace <102531513+HaiderInamWorkSpace@users.noreply.github.com>
2022-07-29 11:13:03 +1000

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className='col-span-2'>
           <p className='uppercase text-xl tracking-widest text-[#5651ef]'>About</p>
           <h2 className='py-2 text-gray-600'>Who I Am</h2>
-          <p className='py-2 text-gray-600'>Passionate and collaborative junior developer poised to build valuable experience and ready to contribute to team success. Well-versed in MERN Stack.</p>
+          <p className='py-2 text-gray-600'>Passionate and collaborative junior developer poised to build valuable experience and ready to contribute to team success. Well-versed in MERN Stack. As a junior software developer, ability to learn new technology quickly, and the ability to work in a team environment. Ultimately, a top-class Junior Software Developer providing support to the design team while continually improving their coding and design skills.</p>
         </div>
         {/* <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl items-center justify-center p-4 hover:scale-105 ease-in duration-300'> */}
         <div className='w-auto h-auto m-auto shadow-gray-400 rounded-xl items-center justify-center hover:scale-105 ease-in duration-300'>
