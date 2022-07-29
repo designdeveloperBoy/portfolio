54766322dfd4236ae98cf250f4415669e481e480
HaiderInamWorkSpace <102531513+HaiderInamWorkSpace@users.noreply.github.com>
2022-07-30 03:48:12 +1000

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import aboutImage from '../public/assets/aboutImage.png'
 
 const About = () => {
   return (
@@ -13,7 +14,7 @@ const About = () => {
         </div>
         {/* <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl items-center justify-center p-4 hover:scale-105 ease-in duration-300'> */}
         <div className='w-full h-auto m-auto rounded-xl items-center justify-center p-4'>
-          <Image className='rounded-xl' src='/../public/assets/aboutImage.png' height='250px' width='250px' alt='/' />
+          <Image className='rounded-xl' src={aboutImage} height='250px' width='250px' alt='/' />
         </div>
       </div>
     </div>
