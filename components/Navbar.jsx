741ef925186aaf87d17bbdd626f50dba49bb5661
HaiderInamWorkSpace <102531513+HaiderInamWorkSpace@users.noreply.github.com>
2022-07-30 04:22:27 +1000

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -103,7 +103,7 @@ const Navbar = () => {
                             <li onClick={() => setNav(false)} className='py-4 text-sm'>Contact</li>
                         </Link>
                     </ul>
-                    <div className='pt-10'>
+                    <div className='pt-5'>
                         <p className='uppercase tracking-widest text-[#5651e5]'>Let&apos;s Connect</p>
                         <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
