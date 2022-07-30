bb23964a4512848eedf5114a70438d5880cf1e4c
HaiderInamWorkSpace <102531513+HaiderInamWorkSpace@users.noreply.github.com>
2022-07-30 11:24:53 +1000

    Update ProjectItem.jsx

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -7,10 +7,11 @@ const ProjectItem = ({title, backgroundImg, projectUrl, keyTools}) => {
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-2 group bg-[#5651e5] hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
         <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' />
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
+            {/* <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3> */}
+            <h3 className='text-lg lg:text-2xl text-white tracking-wider text-center'>{title}</h3>
             <p className='pb-4 pt-2 text-white text-center'>{keyTools}</p>
             <Link href={projectUrl}>
-                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+                <p className='text-center px-4 lg:px-0 py-2 sm:py-3 rounded-lg bg-white text-gray-700 font-bold md:text-lg cursor-pointer'>More Info</p>
             </Link>
         </div>
     </div>
